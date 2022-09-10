567e09bdd69a3dbe98c467c0ffb2ec60ac851734
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-09-10 16:21:02 +0200

    add listener for validation of beer order service

--- src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -11,7 +11,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JmsConfig {
 
     public static final String BREWING_REQUEST_QUEUE = "brewing-request";
-    public static final String NEW_INVENTORY_QUEUE  = "new-inventory";
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+    public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
 
     @Bean
     public MessageConverter messageConverter(ObjectMapper objectMapper){
